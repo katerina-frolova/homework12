a9db1ae95bd142c37b979acb824b92dc756142d9
Екатерина <kate.frolova.16@gmail.com>
2023-02-07 21:50:19 +0300

    Сделано ДЗ

--- src/pro/sky/java/course1/homework12/Author.java
+++ src/pro/sky/java/course1/homework12/Author.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.homework12;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String lastname;
@@ -18,5 +20,21 @@ public class Author {
         return lastname;
     }
 
+    @Override
+    public String toString() {
+        return name + " " + lastname;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && lastname.equals(author.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastname);
+    }
 }
